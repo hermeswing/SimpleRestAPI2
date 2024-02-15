b3d5e123b23e4ebc689ab99eeb4f752b8048f781
ubuntu-park <hermeswing@naver.com>
2024-02-15 21:49:06 +0900

    Comment 수정

--- src/main/java/octopus/base/aop/TrackingAspect.java
+++ src/main/java/octopus/base/aop/TrackingAspect.java
@@ -15,12 +15,24 @@ import org.springframework.stereotype.Component;
 
 /**
  * <pre>
- * MDC(Mapped Diagnostic Context)는 로깅에서 사용되는 패턴으로, 로그 메시지에 특정 데이터를 매핑하여 출력하는데 사용
+ * MDC(Mapped Diagnostic Context)는 Logging에서 사용되는 패턴으로, 로그 메시지에 특정 데이터를 매핑하여 출력하는데 사용
  * Spring AOP와 MDC를 함께 사용하여 각 호출되는 메서드에 대한 정보를 로그에 추가할 수 있습니다.
  *
  * 사용의 예)
  * 로그 출력 시 MDC의 데이터를 사용하도록 logback.xml 또는 log4j2.xml 설정 파일을 구성
- * <property name="LOG_PATTERN" value="%d{yy-MM-dd HH:mm:SSS} [%thread] %-5level %30logger.%method [ %line line ] [%X{ThreadId}] - %msg%n" />
+ * {@code MDC.put( "ThreadId", threadId ); }
+ *
+ * &lt;property name="LOG_PATTERN" value="%d{yy-MM-dd HH:mm:SSS} [%thread] %-5level %30logger.%method [ %line line ] [%X{ThreadId}] - %msg%n" /&gt;
+ *
+ * 다음은 각 부분의 의미입니다:
+ * - %d{yy-MM-dd HH:mm:SSS}: 날짜 및 시간 포맷
+ * - [%thread]: 스레드 이름
+ * - %-5level: 로그 레벨
+ * - %30logger.%method: 로거 이름과 메소드 이름
+ * - [ %line line ]: 라인 번호
+ * - [%X{ThreadId}]: 스레드 ID
+ * - %msg: 로그 메시지
+ * - %n: 줄 바꿈
  *
  * 위 설정에서 %X{ThreadId}는 MDC에서 className 키의 값을 로그 메시지에 추가하도록 합니다.
  * 따라서 각 메서드 호출 시 클래스 이름이 로그에 출력됩니다.
@@ -66,8 +78,7 @@ public class TrackingAspect {
     @Around( "execution(* octopus.*.controller.*.*(..))" )
     public Object loggingControllerAround( ProceedingJoinPoint joinPoint ) throws Throwable {
         try {
-            Object result = joinPoint.proceed();
-            return result;
+            return joinPoint.proceed();
         } finally {
             MyThreadLocal.setTrackingLog( "[Controller Release] " + joinPoint.getSignature().toLongString() );
 
